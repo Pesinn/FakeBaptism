7e7c84694a6208f5bfef395f198e7cfe0260e088
Petur Haukur Johannesson <petur.haukur.johannesson@samskip.com>
2017-06-10 23:13:35 +0000

    Name validator finished in level2

--- Assets/Letters/Scripts/Level2/NameValidator.cs
+++ Assets/Letters/Scripts/Level2/NameValidator.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class NameValidator : MonoBehaviour {
 
     List<bool> _result;
-
 
     public void ValidateName()
     {
@@ -19,11 +19,13 @@ public class NameValidator : MonoBehaviour {
         
         executeOnEachChild(validateObject);
 
-        var str = "";
-        foreach (var i in _result)
-            str += i + " ";
-        Debug.Log(str);
+        // If any element in _result returns false
+        // validateList will become true
+        var validateList = _result.Any(x => x.Equals(false));
 
+        // When validate list is false, we have correct name
+        if (!validateList)
+            executeOnEachChild(setLetterSuccess);
     }
 
     /// <summary>
@@ -39,17 +41,27 @@ public class NameValidator : MonoBehaviour {
         if (obj.childCount == 1)
             _result.Add(compareRelatives(obj, obj.GetChild(0)));
         else if (obj.childCount > 1)
-            Debug.LogError("Object should only have single object");
+            Debug.LogError("Object should only have single object but has " + obj.childCount);
         else
             _result.Add(false);
+    }
+
+    /// <summary>
+    /// Make object green
+    /// </summary>
+    /// <param name="obj">Object to make green</param>
+    private void setLetterSuccess(Transform obj)
+    {
+        if(obj.childCount == 1)
+            obj.transform.GetChild(0).GetComponent<LetterImageColor>().SetLetterSuccess();
+        else
+            Debug.LogError("Object should have single object but has " + obj.childCount);
     }
 
     private void executeOnEachChild(Action<Transform> callback)
     {
         foreach (Transform child in transform)
-        {
             callback(child);
-        }
     }
 
     private bool compareRelatives(Transform parent, Transform child)
